f8b31bb83fb4ab48ff2880f14c7cedb242351265
Manikandan M G <mgmanikandan87@gmail.com>
2013-10-13 20:16:26 +0530

    sample repo

---
+++ Testing/src/com/example/testing/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testing;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello world");
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
